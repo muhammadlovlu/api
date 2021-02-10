59c37b1aad6bf4dca9bfb80384785c66afc351b4
muhammadlovlu <chyshahab@gmail.com>
2021-02-10 19:48:20 +0600

    updated

--- data-load.js
+++ data-load.js
@@ -4,55 +4,55 @@ fetch('https://jsonplaceholder.typicode.com/users')
 
 
 
-// Collectiong & organizing users "REALNAME" through json
-// fetch('http://jsonplaceholder.typicode.com/users')
-//   .then(response => response.json())
-//   .then(json => showRealNames(json))
+Collectiong & organizing users "REALNAME" through json
+fetch('http://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(json => showRealNames(json))
 
-// function showRealNames(names) {
-//   const realNames = names.map(x => x.name);
-//   const nameUL = document.getElementById('name-container');
-//   for (let i = 0; i < realNames.length; i++) {
-//     const realname = realNames[i];
-//     const li = document.createElement("li");
-//     li.innerText = realname;
-//     nameUL.appendChild(li);
-//   }
-// }
-
-
-// // Collectiong & organizing users "EMAIL" through json
-// fetch('http://jsonplaceholder.typicode.com/users')
-//   .then(response => response.json())
-//   .then(json => allMail(json))
-
-// function allMail(usersmail) {
-//   const mailCollect = usersmail.map(x => x.email);
-//   const mailUl = document.getElementById("mail-container");
-//   for (let i = 0; i < mailCollect.length; i++) {
-//     const element = mailCollect[i];
-//     const li = document.createElement("li");
-//     li.innerText = element;
-//     mailUl.appendChild(li);
-//   }
-// }
+function showRealNames(names) {
+  const realNames = names.map(x => x.name);
+  const nameUL = document.getElementById('name-container');
+  for (let i = 0; i < realNames.length; i++) {
+    const realname = realNames[i];
+    const li = document.createElement("li");
+    li.innerText = realname;
+    nameUL.appendChild(li);
+  }
+}
 
 
-// // Collectiong & organizing users "USERNAME" through json
-// fetch('http://jsonplaceholder.typicode.com/users')
-//   .then(response => response.json())
-//   .then(json => displayUser(json))
+// Collectiong & organizing users "EMAIL" through json
+fetch('http://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(json => allMail(json))
 
-// function displayUser(allusers) {
-//   const userNames = allusers.map(x => x.username);
-//   const ul = document.getElementById("username-container");
-//   for (let i = 0; i < userNames.length; i++) {
-//     const username = userNames[i];
-//     const li = document.createElement('li');
-//     li.innerText = username;
-//     ul.appendChild(li);
-//   }
-// }
+function allMail(usersmail) {
+  const mailCollect = usersmail.map(x => x.email);
+  const mailUl = document.getElementById("mail-container");
+  for (let i = 0; i < mailCollect.length; i++) {
+    const element = mailCollect[i];
+    const li = document.createElement("li");
+    li.innerText = element;
+    mailUl.appendChild(li);
+  }
+}
+
+
+// Collectiong & organizing users "USERNAME" through json
+fetch('http://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(json => displayUser(json))
+
+function displayUser(allusers) {
+  const userNames = allusers.map(x => x.username);
+  const ul = document.getElementById("username-container");
+  for (let i = 0; i < userNames.length; i++) {
+    const username = userNames[i];
+    const li = document.createElement('li');
+    li.innerText = username;
+    ul.appendChild(li);
+  }
+}
 
 
 
